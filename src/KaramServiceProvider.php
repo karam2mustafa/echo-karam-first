1cdabb3f5050efd37d5710872067bca97de1e16e
karam2mustafa <56937091+karam2mustafa@users.noreply.github.com>
2020-04-09 01:35:25 +0300

    update
    update

--- src/KaramServiceProvider.php
+++ src/KaramServiceProvider.php
@@ -13,10 +13,10 @@ class KaramServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('command.karam.echo-karam-first' , function ($app){
+        $this->app->singleton('command.karam2mustafa.echo-karam-first' , function ($app){
             return $app['karam\Commands\KaramCommand'];
         });
-        $this->commands('command.karam.echo-karam-first');
+        $this->commands('command.karam2mustafa.echo-karam-first');
     }
 
     /**
